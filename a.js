437b591ee97fe5b4ce420f21a72079e7f49b1cb3
hayeonghh <95855640+cocorig@users.noreply.github.com>
2023-05-19 23:21:26 +0900

    json

---
+++ a.js
@@ -0,0 +1,22 @@
+const a = 
+    //  //띄어쓰기xx {"key" : "value",
+    //               "key" : "value",
+    //                }
+    
+        {  //json은 js와 다름 ->js는 json과 다르게 메소드,undefined를 포함하지않는다.
+            'name': 'kundol'//느슨한 타입이지만 name과 song이 같은 타입으로
+            'like': {
+                '아령':{
+                        'weigth': '10kg',
+                        'feature': '육각형',
+            },
+                '바나나':{
+                        'color': '초록색'
+                    }
+                }
+
+        }
+        //스키마 
+
+    console.log(a.name)
+
